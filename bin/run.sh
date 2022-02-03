ef59d06540396c30b9c3b32d7e99c85f71ad08c2
George Erickson <me@gee.io>
2022-02-03 02:00:56 -0500

    cleanup io_uring code a bit and document races

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 
 rm -f zig-out/bin/*
 zig build -Dtarget=aarch64-linux.5.10.93...5.10.93-gnu.2.26 -Dcpu=neoverse_n1
+
+ssh ec2-user@3.144.255.82 bash -c "pkill -q zig-scratch &> /dev/null || true && rm -f ./zig-scratch"
 scp zig-out/bin/*  ec2-user@3.144.255.82:
-ssh ec2-user@3.144.255.82 bash -c "pkill -q zig-scratch &> /dev/null || true && ./zig-scratch"
+ssh ec2-user@3.144.255.82 bash -c "./zig-scratch"
--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,87 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const assert = std.debug.assert;
+const mem = std.mem;
 const net = std.net;
 const os = std.os;
-const mem = std.mem;
-const assert = std.debug.assert;
+const linux = os.linux;
+const testing = std.testing;
+
+const io_uring_params = linux.io_uring_params;
+const io_uring_sqe = linux.io_uring_sqe;
+const io_uring_cqe = linux.io_uring_cqe;
+
+const TT = union {
+    accept: void,
+    read: os.fd_t,
+};
 
 const Token = packed struct {
     tag: enum(u32) {
         accept,
         read,
+        write,
+        close,
+        shutdown,
     },
-    data: u32,
+    client_fd: os.fd_t = -1,
+};
+
+const Ring = struct {
+    uring: std.os.linux.IO_Uring,
+    client_addr: os.sockaddr,
+    client_addr_len: os.socklen_t = @sizeOf(os.sockaddr),
+
+    pub fn init(entries: u13, flags: u32) !Ring {
+        return Ring{
+            .uring = try std.os.linux.IO_Uring.init(entries, flags),
+            .client_addr = undefined,
+        };
+    }
+
+    fn deinit(self: *Ring) void {
+        self.uring.deinit();
+    }
+
+    fn queue_close(self: *Ring, fd: os.fd_t) !*io_uring_sqe {
+        return try self.uring.close(@bitCast(u64, Token{ .tag = .close }), fd);
+    }
+
+    fn queue_shutdown(self: *Ring, fd: os.socket_t, how: u32) !*io_uring_sqe {
+        return try self.uring.shutdown(@bitCast(u64, Token{ .tag = .close }), fd, how);
+    }
+
+    fn queue_accept(self: *Ring, server: os.socket_t) !*io_uring_sqe {
+        return try self.uring.accept(@bitCast(u64, Token{ .tag = .accept }), server, &self.client_addr, &self.client_addr_len, 0);
+    }
+
+    fn queue_read(
+        self: *Ring,
+        client_fd: os.fd_t,
+        buffer: []u8,
+        offset: u64,
+    ) !*io_uring_sqe {
+        return try self.uring.read(@bitCast(u64, Token{
+            .tag = .read,
+            .client_fd = client_fd,
+        }), client_fd, buffer, offset);
+    }
+
+    fn queue_write(
+        self: *Ring,
+        client_fd: os.fd_t,
+        buffer: []const u8,
+        offset: u64,
+    ) !*io_uring_sqe {
+        const udata = Token{
+            .tag = .write,
+            .client_fd = client_fd,
+        };
+        return try self.uring.write(@bitCast(u64, udata), client_fd, buffer, offset);
+    }
 };
 
 pub fn main() !void {
-
     // TODO: support other tcp-ish socket types
     // - tcp
     // - unix-socket
@@ -27,14 +95,68 @@ pub fn main() !void {
     try os.listen(server, kernel_backlog);
     std.debug.print("net: echo server: io_uring: listening on {}...\n", .{address});
 
-    var ring = try std.os.linux.IO_Uring.init(16, 0);
+    // TODO: no idea what a reasonable value is for entries, 16 seems nice and low.
+    //       smaller is better until benchmarks show otherwise (a big buffer can hide deadlock bugs until prod).
+    var ring = try Ring.init(16, 0);
     defer ring.deinit();
 
-    // var cqes: [512]std.os.linux.io_uring_cqe = undefined;
-    var accept_addr: os.sockaddr = undefined;
-    var accept_addr_len: os.socklen_t = @sizeOf(@TypeOf(accept_addr));
-    _ = try ring.accept(@bitCast(u64, Token{
-        .tag = .accept,
-        .data = 0,
-    }), server, &accept_addr, &accept_addr_len, 0);
+    _ = try ring.queue_accept(server);
+
+    var buffer_read = [_]u8{98} ** 20;
+    var buffer_write = [_]u8{98} ** 20;
+
+    while (true) {
+        // TODO: Write a golang client to fuzz bad behavior from clients (e.g. slow/other).
+        //       long running tests that validate checksums to find weird behavior at scale.
+        // TODO: benchmark vs batching + peeking vs kernal submit thread
+        // TODO: ensure everything else is non-blocking (or attach a timeout?).
+        //       otherwise we need to have a background thread for periodic tasks
+        //       like updating a dns cache or sending metrics/heartbeats.
+        // TODO: install bcc and monitor tcp stuff + other utilization metrics.
+        _ = try ring.uring.submit_and_wait(1);
+
+        const cqe = try ring.uring.copy_cqe();
+        switch (cqe.err()) {
+            .SUCCESS => {},
+            else => |errno| std.debug.panic("unhandled errno: {}", .{errno}),
+        }
+
+        const token = @bitCast(Token, cqe.user_data);
+        std.log.info("token: {}", .{token});
+
+        // tag indicates what event was completed
+        switch (token.tag) {
+            // We have a new connection, we need to
+            // 1) reject new connections over some limit by skipping queue_accept?
+            // 2) dedicate a new "connection" object that has the buffers and other
+            //    stuff dedicated to a single long-lived tcp-connection.
+            .accept => {
+                // TODO: limit number of connections here, by not queing an accept?
+                _ = try ring.queue_accept(server);
+
+                // TODO: I think this has a race condition, if we accept again
+                // before this read has finished the two reads could write to the
+                // buffer concurrently?
+                // We don't control what order the events are completed here.
+                // I think this is solved by using a pool (or dedicated per client)
+                // buffers + readv.
+                _ = try ring.queue_read(cqe.res, buffer_read[0..], @as(u64, 0));
+            },
+            .read => {
+                const data_read = buffer_read[0..@intCast(usize, cqe.res)];
+                std.debug.print("complete.read: {s}\n", .{data_read});
+                _ = try ring.queue_write(token.client_fd, buffer_write[0..], 0);
+            },
+            .write => {
+                // TODO: another ordering bug I think, we need to ensure that no sqe is
+                // still in progress for the given client_fd (I think this can be a flush/nop,
+                // or maybe just a per fd ref count)
+                std.debug.print("complete.write: nbytes={}\n", .{cqe.res});
+                _ = try ring.queue_shutdown(token.client_fd, os.SHUT_RDWR);
+            },
+            .close => {
+                std.debug.print("complete.close\n", .{});
+            },
+        }
+    }
 }
